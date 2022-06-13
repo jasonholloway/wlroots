28880084a3c2299810b500028c9741e6d05c65d6
Jason Holloway <miceeatnicerice@hotmail.com>
2022-06-13 01:17:21 +0100

    ctm client sends ctm fd on start

--- examples/ctm.c
+++ examples/ctm.c
@@ -13,89 +13,104 @@
 struct output {
 	struct wl_output *wl_output;
 	struct zwlr_ctm_control_v1 *control;
-	uint32_t ramp_size;
-	int table_fd;
-	uint16_t *table;
+	double coeffs[9];
+	uint32_t *ctm;
+	int ctm_fd;
 	struct wl_list link;
 };
 
 static struct wl_list outputs;
 static struct zwlr_ctm_manager_v1 *manager = NULL;
 
-/* static int create_anonymous_file(off_t size) { */
-/* 	char template[] = "/tmp/wlroots-shared-XXXXXX"; */
-/* 	int fd = mkstemp(template); */
-/* 	if (fd < 0) { */
-/* 		return -1; */
-/* 	} */
+static int create_anonymous_file(off_t size) {
+	char template[] = "/tmp/wlroots-shared-XXXXXX";
+	int fd = mkstemp(template);
+	if (fd < 0) {
+		return -1;
+	}
 
-/* 	int ret; */
-/* 	do { */
-/* 		errno = 0; */
-/* 		ret = ftruncate(fd, size); */
-/* 	} while (errno == EINTR); */
-/* 	if (ret < 0) { */
-/* 		close(fd); */
-/* 		return -1; */
-/* 	} */
+	int ret;
+	do {
+		errno = 0;
+		ret = ftruncate(fd, size);
+	} while (errno == EINTR);
+	if (ret < 0) {
+		close(fd);
+		return -1;
+	}
 
-/* 	unlink(template); */
-/* 	return fd; */
-/* } */
+	unlink(template);
+	return fd;
+}
 
-/* static int create_gamma_table(uint32_t ramp_size, uint16_t **table) { */
-/* 	size_t table_size = ramp_size * 3 * sizeof(uint16_t); */
-/* 	int fd = create_anonymous_file(table_size); */
-/* 	if (fd < 0) { */
-/* 		fprintf(stderr, "failed to create anonymous file\n"); */
-/* 		return -1; */
-/* 	} */
+static void fill_coeffs(struct output *o, const double saturation) {
+	double base = (1.0 - saturation) / 3.0;
+	for (int i = 0; i < 9; i++) {
+		/* set coefficients. If index is divisible by four (0, 4, 8) add
+			* saturation to coeff
+			*/
+		o->coeffs[i] = base + (i % 4 == 0 ? saturation : 0);
+	}
+}
 
-/* 	void *data = */
-/* 		mmap(NULL, table_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0); */
-/* 	if (data == MAP_FAILED) { */
-/* 		fprintf(stderr, "failed to mmap()\n"); */
-/* 		close(fd); */
-/* 		return -1; */
-/* 	} */
+static void fill_ctm(struct output *o) {
+	for (int i = 0; i < 9; i++) {
+		if (o->coeffs[i] < 0) {
+			o->ctm[i] = (int64_t) (-(o->coeffs[i]) * ((uint64_t) 1L << 32u));
+			o->ctm[i] |= 1ULL << 63u;
+		} else {
+			o->ctm[i] = (int64_t) (o->coeffs[i] * ((uint64_t) 1L << 32u));
+		}
+	}
+}
 
-/* 	*table = data; */
-/* 	return fd; */
-/* } */
 
-/* static void gamma_control_handle_gamma_size(void *data, */
-/* 		struct zwlr_gamma_control_v1 *gamma_control, uint32_t ramp_size) { */
-/* 	struct output *output = data; */
-/* 	output->ramp_size = ramp_size; */
-/* 	output->table_fd = create_gamma_table(ramp_size, &output->table); */
-/* 	if (output->table_fd < 0) { */
-/* 		exit(EXIT_FAILURE); */
-/* 	} */
-/* } */
+static int output_prepare(struct output *o, const double saturation) {
+	int size = 18 * sizeof(uint32_t);
 
-/* static void gamma_control_handle_failed(void *data, */
-/* 		struct zwlr_gamma_control_v1 *gamma_control) { */
-/* 	fprintf(stderr, "failed to set gamma table\n"); */
-/* 	exit(EXIT_FAILURE); */
-/* } */
+	int fd = create_anonymous_file(size);
+	if (fd < 0) {
+		fprintf(stderr, "failed to create anonymous file\n");
+		return -1;
+	}
 
-/* static const struct zwlr_gamma_control_v1_listener gamma_control_listener = { */
-/* 	.gamma_size = gamma_control_handle_gamma_size, */
-/* 	.failed = gamma_control_handle_failed, */
-/* }; */
+	void *data = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+
+	if (data == MAP_FAILED) {
+		fprintf(stderr, "failed to mmap()\n");
+		close(fd);
+		return -1;
+	}
+
+	o->ctm = data;
+	o->ctm_fd = fd;
+
+	fill_coeffs(o, saturation);
+	fill_ctm(o);
+
+	return fd;
+}
+
+
+static void ctm_control_handle_failed(void *data, struct zwlr_ctm_control_v1 *control) {
+	fprintf(stderr, "failed to set ctm\n");
+	exit(EXIT_FAILURE);
+}
+
+static const struct zwlr_ctm_control_v1_listener control_listener = {
+	.failed = ctm_control_handle_failed,
+};
+
 
 
 static void registry_handle_global(void *data, struct wl_registry *registry,
 		uint32_t name, const char *interface, uint32_t version) {
 	if (strcmp(interface, wl_output_interface.name) == 0) {
 		struct output *output = calloc(1, sizeof(struct output));
-		output->wl_output = wl_registry_bind(registry, name,
-			&wl_output_interface, 1);
+		output->wl_output = wl_registry_bind(registry, name, &wl_output_interface, 1);
 		wl_list_insert(&outputs, &output->link);
-	} else if (strcmp(interface,
-			zwlr_ctm_manager_v1_interface.name) == 0) {
-		manager = wl_registry_bind(registry, name,
-			&zwlr_ctm_manager_v1_interface, 1);
+	} else if (strcmp(interface, zwlr_ctm_manager_v1_interface.name) == 0) {
+		manager = wl_registry_bind(registry, name, &zwlr_ctm_manager_v1_interface, 1);
 	}
 }
 
@@ -137,7 +152,7 @@ static const struct wl_registry_listener registry_listener = {
 int main(int argc, char *argv[]) {
 	wl_list_init(&outputs);
 
-	fprintf(stderr, "hello1!");
+	fprintf(stderr, "Starting...\n");
 
 	/* double contrast = 1, brightness = 1, gamma = 1; */
 	/* int opt; */
@@ -175,24 +190,23 @@ int main(int argc, char *argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	/* struct output *output; */
-	/* wl_list_for_each(output, &outputs, link) { */
-	/* 	output->gamma_control = zwlr_gamma_control_manager_v1_get_gamma_control( */
-	/* 		gamma_control_manager, output->wl_output); */
-	/* 	zwlr_gamma_control_v1_add_listener(output->gamma_control, */
-	/* 		&gamma_control_listener, output); */
-	/* } */
-	/* wl_display_roundtrip(display); */
+	struct output *output;
+	wl_list_for_each(output, &outputs, link) {
+		output->control = zwlr_ctm_manager_v1_get_ctm_control(manager, output->wl_output);
+		zwlr_ctm_control_v1_add_listener(output->control, &control_listener, output);
+	}
+	wl_display_roundtrip(display);
 
-	/* wl_list_for_each(output, &outputs, link) { */
-	/* 	fill_gamma_table(output->table, output->ramp_size, */
-	/* 		contrast, brightness, gamma); */
-	/* 	zwlr_gamma_control_v1_set_gamma(output->gamma_control, */
-	/* 		output->table_fd); */
-	/* } */
+	wl_list_for_each(output, &outputs, link) {
+		if(output_prepare(output, 2) < 0) {
+			exit(EXIT_FAILURE);
+		}
+
+		zwlr_ctm_control_v1_set_ctm(output->control, output->ctm_fd);
+	}
 
 	while (wl_display_dispatch(display) != -1) {
-		// This space is intentionnally left blank
+		// This space is intentionally left blank
 	}
 
 	return EXIT_SUCCESS;
